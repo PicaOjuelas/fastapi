dfd12548ba6ef02e2023a8b112c5873c7b1a139e
Pepe <pepe.bonilla5@gmail.com>
2024-10-29 18:12:15 -0700

    init commit
    Co-authored-by: Sergio Junior Castro Egurrola <SergioJuniorCE@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-print("ola putos")
+from typing import Union
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"soy puto":"si soy jaja"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+@app.post("/login")
+
+def login(body):
+    username = body.username
+    password = body.password
+
